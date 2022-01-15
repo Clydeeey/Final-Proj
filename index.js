39aca6f5e0babab8eab5774cff3d88eee6f20cfb
Izhar Cajilig <72139837+noycajilig@users.noreply.github.com>
2022-01-15 21:26:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ function factorial () {
     if (number < 0) {
         document.getElementById('result').innerHTML = `Error!`
     }
+
+    else if (number === 0) {
+        document.getElementById('result').innerHTML = `The factorial of ${number} is 1.`
+    }
+
     else {
         let result = 1;
         for (i = 1; i <= number; i++) {
@@ -14,6 +19,11 @@ function factorial () {
 
 function fibonacci () {
     const number = document.getElementById('fibonacciNumber').value
+    if (number === 0) {
+        document.getElementById('result2').innerHTML = '0'
+    }
+
+    else {
     let f1 = 0
     let f2 = 1
     let sequence = []
@@ -24,20 +34,6 @@ function fibonacci () {
         f1 = f2
         f2 = sequence
     }
-    }
-
-
-function euclidean () {
-    let num1 = document.getElementById('input1').value;
-    let num2 = document.getElementById('input2').value;
-
-    let euclid = []
-    while ((num1 % num2) > 0) {
-        euclid = num1 % num2 
-        num1 = num2
-        num2 = euclid
-        document.getElementById('euclideanResult').innerHTML = euclid
-        console.log(euclid)
     }
 }
 function percentage() {
